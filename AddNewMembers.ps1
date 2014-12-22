a87d5c26fb7a600d5025d01b64ea46fe8b228bf7
rig4red <jsspfr@gmail.com>
2014-12-22 11:25:36 -0800

    test

---
+++ AddNewMembers.ps1
@@ -0,0 +1,149 @@
+﻿### SCRIPT NAME:  AddNewMembers.ps1
+###
+### DATE UPDATED: 12/01/2014
+###
+### DEPENDENCIES: CreateAcuUser.ps1
+###               fimtoswe.csv
+###
+### AUTHOR:       Jesse Pifer (v-jpifer@microsoft.com)
+###
+### DESCRIPTION:  Get list of users with ACU entitlements from FIM (fimtoswe.csv) and compare against
+###               current ACU users in ADUC. Any users with FIM ACU entitlements that do not exist in
+###               ADUC are created and the username/password outputted to screen to be sent to user
+###               via encrypted e-mail.
+
+
+ipmo activedirectory
+
+$scriptpath = $MyInvocation.MyCommand.Path
+$dir = Split-Path $scriptpath
+$fromfim = Get-Content -Path "$dir\fimtoswe.csv"
+$path = "$dir\toAD.csv"
+
+If ((Test-Path $path) -eq $true) { Clear-Content -Path $path }
+"{0},{1}" -f "ACU","Alias" | add-content -Path $path
+
+$ACUs = @("SWE TST ACU","SWE SEC ACU","SWE RWC ACU","SWE RSW ACU","SWE RSP ACU","SWE AZU ACU", "SWE IDT ACU", "SWE IMG ACU", "SWE PLT ACU","SWE REX ACU","SWE RFF ACU","SWE RLY ACU","SWE RNZ ACU","SWE ROD ACU")
+
+Foreach ($ACU in $ACUs)
+{
+
+    Switch ($ACU)
+        {
+        "SWE AZU ACU" {$acronym = "AZU"}
+        "SWE IDT ACU" {$acronym = "IDT"}
+        "SWE IMG ACU" {$acronym = "IMG"}
+        "SWE PLT ACU" {$acronym = "PLT"}
+        "SWE REX ACU" {$acronym = "REX"}
+        "SWE RFF ACU" {$acronym = "RFF"}
+        "SWE RLY ACU" {$acronym = "RLY"}
+        "SWE RNZ ACU" {$acronym = "RNZ"}
+        "SWE ROD ACU" {$acronym = "ROD"}
+        "SWE RSP ACU" {$acronym = "RSP"}
+        "SWE RSW ACU" {$acronym = "RSW"}
+        "SWE RWC ACU" {$acronym = "RWC"}
+        "SWE SEC ACU" {$acronym = "SEC"}
+        "SWE TST ACU" {$acronym = "TST"}
+        }
+
+    $ACUIndex = $fromfim.IndexOf($ACU)
+    $i = 1
+    $name = $fromfim[$ACUIndex + $i]
+
+    Write-Host "Checking $ACU" -BackgroundColor DarkCyan
+
+    while ($name -ne "")
+    {
+        #Write-Host "Checking $name"
+        $filter = "displayName -like `"$name`""
+        $alias = Get-ADUser -Filter $filter -SearchBase "OU=Managed,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net" | Select -ExpandProperty SamAccountName
+   
+        if (($alias -ne $null) -and (-not($alias -like "*_ratst*")))
+        {
+            $aliasacu = $alias + "_" + $acronym
+            $filter = "sAMAccountName -like `"$aliasacu`""
+            $validateUser = Get-ADUser -Filter $filter | Select -ExpandProperty sAMAccountName
+
+            if ($validateUser -eq $null)
+            {
+                Write-Host "Creating $acronym - $name - $alias" -ForegroundColor "Green"
+                "{0},{1}" -f $acronym,$alias | add-content -Path $path
+            }
+        }
+    
+        else
+        {
+            if (-not($alias -like "*_ratst*"))
+            {
+                Write-Host "$name has not created their basic SWE account" -ForegroundColor "RED"
+            }
+        }
+
+        $i++
+        $name = $fromfim[$ACUIndex + $i]
+    }
+
+}
+
+
+$toCreate = Import-Csv -Path $path
+
+if ($toCreate -ne $null)
+{
+    Write-Host "#                    CREATED                    #" -BackgroundColor Green
+    Foreach ($user in $toCreate)
+    {
+       & $dir\CreateAcuUser.ps1 -user $user.Alias -acu $user.ACU
+    }
+}
+
+Write-Host "`n"
+
+$AzureTeams = @("SWE Azure BOX Team","SWE Azure BXA Team","SWE Azure BXT Team","SWE Azure BXC Team","SWE Azure DC On Call Engineers","SWE Azure SLAM On Call Engineers","SWE Azure SUE On Call Engineers")
+
+Foreach ($team in $AzureTeams)
+{
+    $TeamIndex = $fromfim.IndexOf($team)
+    $i = 1
+    $name = $fromfim[$TeamIndex + $i]
+
+    $membership = Get-ADGroupMember -Identity $team
+
+    Write-Host "Checking $team" -BackgroundColor DarkCyan
+
+    while ($name -ne "")
+    {
+        $filter = "displayName -like `"$name`""
+        $alias = Get-ADUser -Filter $filter -SearchBase "OU=Managed,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net" | Select -ExpandProperty SamAccountName
+           
+        if (($alias -ne $null) -and (-not($alias -like "*_ratst*")))
+        {
+            $alias += "_azu"
+            if ((Get-ADUser -Filter {SamAccountName -like $alias} | Select -ExpandProperty SamAccountName) -ne $null)
+            {            
+                if (-not($membership.SamAccountName -contains $alias))
+                {
+                    Write-Host "Adding $alias"
+                    Add-ADGroupMember -Identity $team -Members $alias
+                }
+            }
+            else
+            {
+                Write-Host "$alias does not exist"
+            }
+        }
+        
+        else
+        {
+            if (-not($alias -like "*_ratst*"))
+            {
+                Write-Host "$name has not created their basic SWE account" -ForegroundColor "RED"
+            }
+        }
+
+        $i++
+        $name = $fromfim[$TeamIndex + $i]
+    }
+}
+
+Write-Host "Script complete"
---
+++ CreateAcuUser.ps1
@@ -0,0 +1,136 @@
+﻿### SCRIPT NAME:  CreateAcuUser.ps1
+###
+### DATE UPDATED: 06/13/2014
+###
+### AUTHOR:       Jesse Pifer (v-jpifer@microsoft.com)
+###
+### DESCRIPTION:  Creates an ACU account based off inputed paramaters $user and $acu.
+###               After creating the account the username and password are write-hosted
+###               to the console.
+
+
+Param(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$user,
+
+    [Parameter(Mandatory=$True)]
+    [string]$acu
+)
+
+ipmo activedirectory
+
+Function GET-Temppassword() {
+
+# GET-Temppassword found at:
+# http://blogs.technet.com/b/heyscriptingguy/archive/2013/06/03/generating-a-new-password-with-windows-powershell.aspx
+	
+	$ascii=$NULL;For ($a=33;$a –le 126;$a++) {$ascii+=,[char][byte]$a }
+		
+	For ($loop=1; $loop –le 15; $loop++) {
+
+	    $TempPassword+=($ascii | GET-RANDOM)
+
+	}
+
+	return $TempPassword
+}
+
+switch ($acu)
+{
+	"REX" { 	$groupDisplayName = " - Remote Access (REX)"
+				$groupAcronym = "_rex"
+				$groupName = "CN=SWE Remote Access Users (REX),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"RFF" { 	$groupDisplayName = " - Remote Access (RFF)"
+				$groupAcronym = "_rff"
+				$groupName = "CN=SWE Remote Access Users (RFF),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"RLY" { 	$groupDisplayName = " - Remote Access (RLY)"
+				$groupAcronym = "_rly"
+				$groupName = "CN=SWE Remote Access Users (RLY),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"ROD" { 	$groupDisplayName = " - Remote Access (ROD)"
+				$groupAcronym = "_rod"
+				$groupName = "CN=SWE Remote Access Users (ROD),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+
+	"RNZ" {     $groupDisplayName = " - Remote Access (RNZ)"
+        		$groupAcronym = "_rnz"
+                $groupName = "CN=SWE Remote Access Users (RNZ),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+                $PW = GET-Temppassword
+                $secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+                        		
+	"RSP" { 	$groupDisplayName = " - Remote Access (RSP)"
+				$groupAcronym = "_rsp"
+				$groupName = "CN=SWE Remote Access Users (RSP),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"RSW" { 	$groupDisplayName =  " - Remote Access (RSW)"
+				$groupAcronym = "_rsw"
+				$groupName = "CN=SWE Remote Access Users (RSW),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"RWC" { 	$groupDisplayName = " - Remote Access (RWC)"
+				$groupAcronym = "_rwc"
+				$groupName = "CN=SWE Remote Access Users (RWC),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"TST" { 	$groupDisplayName = " - Remote Access (TST)"
+				$groupAcronym = "_tst"
+				$groupName = "CN=SWE Remote Access Users (TST),OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"AZU" { 	$groupDisplayName = " - Azure"
+				$groupAcronym = "_azu"
+				$groupName = "" #"CN=SWE Azure BOX Team,OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"IDT" { 	$groupDisplayName = " - Remote Access (IDT)"
+				$groupAcronym = "_idt"
+				$groupName = "CN=SWE Identity Team,OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"IMG" { 	$groupDisplayName = " - Imaging"
+				$groupAcronym = "_img"
+				$groupName = "CN=SWE Imaging Team,OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"PLT" { 	$groupDisplayName = " - Platform"
+				$groupAcronym = "_plt"
+				$groupName = "CN=SWE Platform Team,OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+		
+	"SEC" { 	$groupDisplayName = " - Security"
+				$groupAcronym = "_sec"
+				$groupName = "CN=SWE Security Team,OU=Role Groups,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net"
+				$PW = GET-Temppassword
+				$secure = ConvertTo-SecureString $PW -AsPlainText -Force }
+}
+ 
+$userObject = Get-ADUser -LDAPFilter "(sAMAccountName=$user)"
+$newSam = $userObject.SamAccountName + $groupAcronym
+$newDisplayName = $userObject.GivenName + " " + $userObject.Surname + $groupDisplayName
+New-ADUser -SamAccountName $newSam -Name $newDisplayName -GivenName $userObject.GivenName -Surname $userObject.Surname -CannotChangePassword $false -DisplayName $newDisplayName -Path "OU=Unmanaged,OU=Accounts,DC=swe,DC=prd,DC=msft,DC=net" -Enabled $true -ChangePasswordAtLogon $true -PasswordNeverExpires $false -AccountPassword $secure
+
+$newUserObject = Get-ADUser -LDAPFilter "(sAMAccountName=$newSam)"
+$dName = $newUserObject.DistinguishedName
+$newUser = Get-ADUser $dName -Server "swe.prd.msft.net"
+$groupN = Get-ADGroup "$groupName" -Server "swe.prd.msft.net"
+Add-ADGroupMember $groupN -Members $newUser -Server "swe.prd.msft.net"
+
+write-host $newSam " " $PW
